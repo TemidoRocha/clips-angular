552da2d428d2a7e20b180db8074aad2abdb8c5aa
Manuel Rocha <manuel.rocha@ctw.bmwgroup.com>
2022-11-13 20:49:01 +0000

    reactive forms chapter completed

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -13,9 +13,24 @@ export class RegisterComponent {
   // if we use the validators directly on html angular will assume those values, check email minLength
   email = new FormControl('', [Validators.required, Validators.email]);
   age = new FormControl('', [Validators.min(18), Validators.max(120)]);
-  password = new FormControl('');
-  confirm_password = new FormControl('');
-  phone_number = new FormControl('');
+  password = new FormControl('',
+    [
+      Validators.required,
+      Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
+    ]);
+  confirm_password = new FormControl('', [
+    Validators.required
+  ]);
+  phone_number = new FormControl('',
+    [
+      Validators.required,
+      Validators.maxLength(9),
+      Validators.minLength(9)
+    ]);
+
+  showAlert = false
+  alertMsg = 'Please wait! Your account is being created.'
+  alertColor = 'blue'
 
   // FormGroup is needed to isolate different forms in the same page
   registerForm = new FormGroup({
@@ -26,4 +41,10 @@ export class RegisterComponent {
     confirm_password: this.confirm_password,
     phone_number: this.phone_number,
   });
+
+  register() {
+    this.showAlert = true;
+    this.alertMsg = 'Please wait! Your account is being created.';
+    this.alertColor = 'blue';
+  }
 }
